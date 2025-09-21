5ab7dee61d7486641cc49de2161e32025f5ef9d6
Dora Babu Bandla <dorababu.bandla@zelis.com>
2025-09-21 12:42:07 +0530

    add example controller

---
+++ Controllers/Example.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bulkyweb.Controllers
+{
+    public class Example : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
